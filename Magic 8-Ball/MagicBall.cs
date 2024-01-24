7f0bb894c411ca2eb08ab5a40067b631b2a1bfac
Zach H <huntzachary30@gmail.com>
2024-01-23 19:18:13 -0600

    Finished program.

---
+++ Magic 8-Ball/MagicBall.cs
@@ -0,0 +1,33 @@
+﻿namespace Magic_8_Ball
+{
+    internal class MagicBall
+    {
+        //Fields
+        private string[] _phrases;
+        private Random _random;
+
+        //Constructor
+        public MagicBall()
+        {
+            try
+            {
+                //Read the answers from the file
+                _phrases = File.ReadAllLines("src/Phrases.txt");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                _phrases = new string[] { "No phrases found. Check file input." };
+            }
+
+            _random = new Random();
+        }
+
+        //Getters
+        public string GetRandomPhrase()
+        {
+            return _phrases[_random.Next(_phrases.Length)];
+        }
+    }
+}
--- Magic 8-Ball/Program.cs
+++ Magic 8-Ball/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using Magic_8_Ball;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MagicBall magicBall = new();
+
+        Console.WriteLine("Welcome to the Magic 8 Ball!");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+
+        while (true)
+        {
+            Console.WriteLine("Ask me a question and I will tell you the future!");
+            Console.Write("Question: ");
+            Console.ReadLine();
+
+            Console.WriteLine(magicBall.GetRandomPhrase());
+
+            Console.WriteLine("Do you have another question for the Magic 8 Ball? (y/n)");
+            string answer = Console.ReadLine()!;
+
+            while (true)
+            {
+                if (answer!.ToLower() == "y" || answer.ToLower() == "n") break;
+                Console.WriteLine("Invalid input. Please enter 'y' or 'n'.");
+                answer = Console.ReadLine()!;
+            }
+
+            if (answer.ToLower() != "y") break;
+
+            Console.Clear();
+        }
+
+        Console.Clear();
+
+        Console.WriteLine("Thank you for playing with the magic 8-Ball!");
+    }
+}
